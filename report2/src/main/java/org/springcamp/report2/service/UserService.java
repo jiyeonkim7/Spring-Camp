cb5108db36acc0765b33e6c69a6a6b83d1695497
jiyeonkim7 <jiyean9629@gmail.com>
2018-11-11 23:54:20 +0900

    update report2

---
+++ report2/src/main/java/org/springcamp/report2/service/UserService.java
@@ -0,0 +1,8 @@
+package org.springcamp.report2.service;
+
+import org.springcamp.report2.model.User;
+
+public interface UserService {
+
+    User getByUserIdx(final int user_idx);
+}
---
+++ report2/src/main/java/org/springcamp/report2/service/impl/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package org.springcamp.report2.service.impl;
+
+import org.springcamp.report2.model.User;
+import org.springcamp.report2.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public User getByUserIdx(final int user_idx){
+
+        User user = new User();
+
+
+        return user;
+    }
+}
